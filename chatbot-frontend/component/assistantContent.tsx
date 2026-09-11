4be2dd23a78c55d4d245f7cce5ba7ecc69f192b5
Puneet <puneet987720@gmail.com>
2026-09-11 19:19:55 +0530

    text markdown

--- chatbot-frontend/component/assistantContent.tsx
+++ chatbot-frontend/component/assistantContent.tsx
@@ -1,9 +1,10 @@
 import { View, Text } from "react-native";
+import Markdown from "react-native-markdown-display";
 
 export default function AssistantContent({ response }: { response: string }) {
   return (
     <View className="text-lg flex-auto items-center h-auto">
-      <Text className="text-lg color-white">{response}</Text>
+      <Markdown style={{body: { color: "white", fontSize: 20 }}}>{response}</Markdown>
     </View>
   );
 }
